ce315f62ca85a4ae8b7f4a5ec2e2edd6f37b112d
sasha <sasha_kobzev18@mail.ru>
2023-11-04 03:04:34 +0300

    refactor and adds colors

--- app/src/main/java/com/example/yesnot/MainActivity.java
+++ app/src/main/java/com/example/yesnot/MainActivity.java
@@ -1,10 +1,9 @@
 package com.example.yesnot;
 
-import static java.security.AccessController.getContext;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
@@ -18,6 +17,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Objects;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -34,30 +34,11 @@ public class MainActivity extends AppCompatActivity {
         generate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                if (NetworkMenager.isNetworkAvailable(getBaseContext())){
-                    String url = "https://www.random.org/integers/?num=1&min=1&max=10000&col=1&base=10&format=plain&rnd=new";
-                    new GetUrlData().execute(url);
-                }
-                else{
-                    Toast toast = Toast.makeText(getApplicationContext(), "No internet connecton", Toast.LENGTH_SHORT);
-                    toast.show();
-                    Random random = new Random();
-                    result.setText(AnswerMeager.isEven(random.nextInt(1000) + 1));
-                }
+                new GetAnswer().execute();
             }
         });
     }
-    public static class AnswerMeager{
-        public static String isEven(int number){
-            if(number % 2 == 0){
-                return "Yes";
-            }
-            else {
-                return "No";
-            }
-        }
-    }
+
     public static class NetworkMenager{
         public static boolean isNetworkAvailable(Context context){
             ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -65,18 +46,22 @@ public class MainActivity extends AppCompatActivity {
             return networkInfo != null && networkInfo.isConnected();
         }
     }
-    private class GetUrlData extends AsyncTask<String, String, String> {
-        protected void onPreExecute(){
-            super.onPreExecute();
-            result.setText("Wait...");
+    private static class AnswerMenager{
+        public static String  isEven(int number){
+            if(number % 2 == 0){
+                return "Yes";
+            }
+            else {
+                return "No";
+            }
         }
-        @Override
-        protected String doInBackground(String... strings) {
+        public static String  ApiAnswer(){
+           String url = "https://www.random.org/integers/?num=1&min=1&max=10000&col=1&base=10&format=plain&rnd=new";
             String reply = null;
             HttpURLConnection connection = null;
 
             try {
-                connection = (HttpURLConnection) new URL(strings[0]).openConnection();
+                connection = (HttpURLConnection) new URL(url).openConnection();
 
                 connection.setRequestMethod("GET");
                 connection.setUseCaches(false);
@@ -98,12 +83,57 @@ public class MainActivity extends AppCompatActivity {
                     connection.disconnect();
                 }
             }
-            return reply;
+            if (reply != null){
+                return isEven(Integer.parseInt(reply));
+            }
+            else {
+                return "error";
+            }
+
+        }
+        public static String RandomAnswer(){
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            Random random = new Random();
+            return isEven(random.nextInt(1000) + 1);
+        }
+
+    }
+    private class GetAnswer extends AsyncTask<String, String, String> {
+        protected void onPreExecute(){
+            super.onPreExecute();
+            result.setTextColor(Color.WHITE);
+            result.setText("Wait...");
+        }
+        @Override
+        protected String doInBackground(String... strings) {
+            String reply = null;
+            if(NetworkMenager.isNetworkAvailable(getBaseContext())){
+                reply = AnswerMenager.ApiAnswer();
+                return reply;
+            }
+            else{
+                reply = AnswerMenager.RandomAnswer();
+                return reply;
+            }
         }
         @Override
         protected void onPostExecute(String reply){
             super.onPostExecute(reply);
-            result.setText(AnswerMeager.isEven(Integer.parseInt(reply)));
+            if(!NetworkMenager.isNetworkAvailable(getBaseContext())){
+                Toast toast = Toast.makeText(getApplicationContext(), "No internet connecton", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+            if(Objects.equals(reply, "Yes")){
+                result.setTextColor(Color.GREEN);
+            }
+            else {
+                result.setTextColor(Color.RED);
+            }
+            result.setText(reply);
         }
     }
 }
